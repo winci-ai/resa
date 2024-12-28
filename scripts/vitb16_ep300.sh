e9e46a0bb41b81a470a04045dac6ea8f487f17c6
winci-ai <winci@buaa.edu.cn>
2024-12-28 21:40:08 +0800

    fix

---
+++ scripts/vitb16_ep300.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+port=10001
+gpu=0,1,2,3
+epochs=300
+arch=vit_base
+patch_size=16
+bs=256
+gpu_list=(${gpu//,/ })
+ngpu=${#gpu_list[@]}
+bs_total=$((bs * ngpu))
+env_name="resa_${arch}-${patch_size}_ep${epochs}_bs${bs_total}"
+dump_path="out/${env_name}"
+
+CUDA_VISIBLE_DEVICES=${gpu} torchrun --nproc_per_node=${ngpu} --master_port=${port} main.py \
+--arch ${arch} \
+--patch_size ${patch_size} \
+--epochs ${epochs} \
+--batch_size ${bs} \
+--lr ${lr} \
+--data_path ./data/ImageNet/ \
+--dump_path ${dump_path} \
