f7b785d9e13dd633fb1ed8e828970869238c30c1
winci-ai <winci@buaa.edu.cn>
2025-03-09 14:49:45 +0800

    Initial commit

--- src/vision_transformer.py
+++ src/vision_transformer.py
@@ -167,8 +167,7 @@ class PatchEmbed(nn.Module):
         self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
 
     def forward(self, x):
-        x = self.proj(x).flatten(2).transpose(1, 2)
-        return x
+        return self.proj(x).flatten(2).transpose(1, 2)
     
 class ConvEmbed(nn.Module):
     """ ConvStem that supports variable patch sizes """
